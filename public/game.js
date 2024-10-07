00cdf44cb9769280a0b448aae631641453326fac
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-10-08 01:35:05 +0200

    Update game.js

--- public/game.js
+++ public/game.js
@@ -28,7 +28,7 @@ class Character {
         this.energy = this.maxEnergy;
         learnRandomAbility(this);
         console.log("Level up:", this);
-        
+        showLevelUpModal()
     }
 
     useAbility(abilityIndex, target) {
