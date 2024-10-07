682683a87d83ba9dd83515499132552a55a260d4
Spik2607 <97451503+Spik2607@users.noreply.github.com>
2024-10-07 15:51:00 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware pour servir les fichiers statiques
+app.use(express.static('public'));
+
+// Route de base
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur votre serveur Node.js!');
+});
+
+// Démarrage du serveur
+app.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
